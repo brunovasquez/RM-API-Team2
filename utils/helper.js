7237f41c6246834d71a240ed4a78ae1054bbebf5
AlvaroDazaQuiroga <Alvaro.Daza@fundacion-jala.org>
2016-09-07 21:08:37 -0400

    improving code

--- utils/helper.js
+++ utils/helper.js
@@ -3,15 +3,34 @@
  * @param roomServiceResult room service list from the endpoint
  * @param roomsServiceListExpected Room service list from the database
  */
-var compareRoomService = function(roomServiceResult, roomsServiceListExpected){
+var compareRoomService = function (roomServiceResult, roomsServiceListExpected) {
     var totalFound = 0;
-    roomServiceResult.forEach(elementNow =>
-    {totalFound += roomsServiceListExpected.filter(elementExpect =>
-    elementNow._id == elementExpect._id &&
-    elementNow.emailAddress == elementExpect.emailAddress &&
-    elementNow.displayName == elementExpect.displayName &&
-    elementNow.serviceId == elementExpect.serviceId).length
+    roomServiceResult.forEach(elementNow => {
+        totalFound += roomsServiceListExpected.filter(elementExpect =>
+        elementNow._id == elementExpect._id &&
+        elementNow.emailAddress == elementExpect.emailAddress &&
+        elementNow.displayName == elementExpect.displayName &&
+        elementNow.serviceId == elementExpect.serviceId).length
     });
     return totalFound;
-}
+};
 exports.compareRoomService = compareRoomService;
+/**
+ * This function is used to do the validation given an a requested body
+ * and the function returns the quantity
+ * @param actualResult request of out of orders
+ * @param resultExpected  the request resultant
+ * @returns {number} the quantity
+ */
+var compareAllOutOfOrders = function (actualResult, resultExpected) {
+    var totalPresents = 0;
+    actualResult.forEach(element => {
+        totalPresents += resultExpected.filter(dbElement => element._id == dbElement._id &&
+        element.roomId == dbElement.roomId &&
+        element.title == dbElement.title &&
+        element.from == new Date(dbElement.from).toISOString()).length
+
+    });
+    return totalPresents;
+};
+exports.compareAllOutOfOrders=compareAllOutOfOrders;
